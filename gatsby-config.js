c9f67d3c7c4108240c3a55fb71bc25913a3fe635
Jan Gottweiss <jan.gottweiss@sap.com>
2019-08-16 15:11:36 +0200

    load remark plugins for mdx

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,6 +14,23 @@ module.exports = {
       resolve: `gatsby-plugin-mdx`,
       options: {
         extensions: [`.mdx`, `.md`],
+        gatsbyRemarkPlugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 1920,
+            },
+          },
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          `gatsby-remark-prismjs`,
+          `gatsby-remark-copy-linked-files`,
+          `gatsby-remark-smartypants`,
+        ],
       }
     },
     {
